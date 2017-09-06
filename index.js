e9f3147cc573363903d21ef7829323ca071bfd54
Nathan Campbell <nathancampbell@outlook.com>
2017-09-06 16:04:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested .target')
 }
+
+function increaseRankBy(n) {
+  var lists = document.querySelectorAll('.ranked-list') {
+    for (let i = 0; i < lists.length; i++) {
+      let children = lists[i].children
+
+      for (let c = 0; c < children.length; c++) {
+        children[c].innerHTML = parseInt(children[c].innerHTML) + n
+      }
+    }
+  }
+}
